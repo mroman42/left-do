e56c48ef975c18919cf833d515c8f30ccd060c6a
Mario Román <mromang08@gmail.com>
2026-02-12 17:19:37 +0200

    Monads are now functors

--- monad.rkt
+++ monad.rkt
@@ -6,19 +6,24 @@
 ;; implementations for multiple basic monads (List, Identity, Maybe, ...).
 ;; The distribution monad is implemented separately (subdistributions.rkt).
 
-(struct monad (return bind))
+(struct monad (return bind map))
 
 ;; List monad.
 (define List
   (monad
+   ;; x -> List x
    (λ (x) (list x))
-   (λ (xs f) (append-map f xs))))
+   ;; List x -> (x -> List y) -> List y
+   (λ (xs f) (append-map f xs))
+   ;; (x -> y) -> List x -> List y
+   (λ (f xs) (map f xs))))
 
 ;; Identity monad.
 (define Id
   (monad
    (λ (x) x)
-   (λ (xs f) (f xs))))
+   (λ (xs f) (f xs))
+   (λ (f xs) (f xs))))
 
 ;; Maybe monad.
 (struct just (elem) #:transparent)
@@ -32,19 +37,24 @@
    (λ (xs f)
      (if (just? xs)
          (f (just-elem xs))
+         (nothing)))
+   ;; map : (x -> y) -> (Maybe x -> Maybe y)
+   (λ (f mx)
+     (if (just? mx)
+         (just (f (just-elem mx)))
          (nothing)))))
 
 
 ;; Continuation monad.
-(define Cont
-  (monad 
+;(define Cont
+ ; (monad 
    ;; return : x -> (x -> r) -> r
-   (λ (x) (λ (f) (f x)))
+  ; (λ (x) (λ (f) (f x)))
    ;; bind : ((x -> r) -> r) ->
    ;;        (x -> ((y -> r) -> r)) ->
    ;;        ((y -> r) -> r)
-   (λ (d f) (λ (k) (d (λ (x) ((f x) k)))))
-   ))
+  ; (λ (d f) (λ (k) (d (λ (x) ((f x) k)))))
+  ; ))
    
 (provide (struct-out monad))
 (provide List)
@@ -52,4 +62,4 @@
 (provide (struct-out just))
 (provide Maybe)
 (provide Id)
-(provide Cont)
+;(provide Cont)
